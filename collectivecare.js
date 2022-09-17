a6affce99dce0cb100ad751691e343615368aaa4
shiro0207 <15901194779@163.com>
2022-09-17 23:49:51 +0800

    9.17

---
+++ collectivecare.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $( "img" ).hover(function() {
+        $("img").css('height','70vh');
+        $("img").css('margin-top','15vh');
+        $("body").css('opacity','0.2');
+    });
